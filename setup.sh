3d9bfd9f66defd56f53f8e40513b4942e3f502b7
dmessent <davidmessent@gmail.com>
2023-02-19 22:52:06 -0800

    Don't do a bunch of the stuff in this script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
 # Set new user password
-echo "First, let's set a new password for better security!"
-passwd
+# echo "First, let's set a new password for better security!"
+# passwd
 
 # Enable SSH
-sudo systemctl enable ssh
-sudo systemctl start ssh
+# sudo systemctl enable ssh
+# sudo systemctl start ssh
 
 # Prepare system
 sudo apt-get update -y
@@ -14,12 +14,12 @@ sudo apt-get upgrade -y
 sudo apt-get install git ruby -y
 
 # Clone this repo
-git clone https://github.com/sinedied/pi-midi-host
-cd pi-midi-host
+git clone https://github.com/dmessent/pi-midi-host-no-bt
+cd pi-midi-host-no-bt
 
 # Optimize for power efficiency and fast boot
-sudo cp config.txt /boot/
-sudo cp cmdline.txt /boot/
+# sudo cp config.txt /boot/
+# sudo cp cmdline.txt /boot/
 
 # Make device identifiable more easily on the network
 sudo apt-get install avahi-daemon -y
@@ -40,23 +40,23 @@ sudo systemctl start midi.service
 sudo apt-get install midisport-firmware -y
 
 # Setup MIDI bluetooth
-git clone https://github.com/oxesoft/bluez
-sudo apt-get install -y autotools-dev libtool autoconf
-sudo apt-get install -y libasound2-dev
-sudo apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
-cd bluez
-./bootstrap
-./configure --enable-midi --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var
-make
-sudo make install
-cd ..
-sudo cp 44-bt.rules /etc/udev/rules.d/
-sudo udevadm control --reload
-sudo service udev restart
-sudo cp btmidi.service /lib/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable btmidi.service
-sudo systemctl start btmidi.service
+# git clone https://github.com/oxesoft/bluez
+# sudo apt-get install -y autotools-dev libtool autoconf
+# sudo apt-get install -y libasound2-dev
+# sudo apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
+# cd bluez
+# ./bootstrap
+# ./configure --enable-midi --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var
+# make
+# sudo make install
+# cd ..
+# sudo cp 44-bt.rules /etc/udev/rules.d/
+# sudo udevadm control --reload
+# sudo service udev restart
+# sudo cp btmidi.service /lib/systemd/system/
+# sudo systemctl daemon-reload
+# sudo systemctl enable btmidi.service
+# sudo systemctl start btmidi.service
 
 # Create alias to show connected devices
 echo >> ~/.bashrc
@@ -69,16 +69,16 @@ echo "alias connect='connectall.rb'" >> ~/.bashrc
 echo >> ~/.bashrc
 
 # Make FS read-only to avoid SD card corruption
-git clone https://gitlab.com/larsfp/rpi-readonly
-cd rpi-readonly
-sudo ./setup.sh -y
-cd ..
+# git clone https://gitlab.com/larsfp/rpi-readonly
+# cd rpi-readonly
+# sudo ./setup.sh -y
+# cd ..
 
 # Turn on read-only mode
 # Use command "rw" to enable writes again
-ro
+# ro
 
-echo "The system will now reboot in read-only mode."
-echo "Use command \"rw\" to enable writes again."
-read -p "Press [enter] to reboot"
-sudo reboot
+# echo "The system will now reboot in read-only mode."
+# echo "Use command \"rw\" to enable writes again."
+# read -p "Press [enter] to reboot"
+# sudo reboot
